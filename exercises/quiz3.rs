b45a08ff1357b8e93fe63faf9e7143f33a8c00f1
tinyXin <wxliny@gmail.com>
2024-05-25 23:42:58 +0800

    wip: 17_test

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,10 +16,12 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
+
+use core::fmt;
 
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: String,
     pub student_name: String,
     pub student_age: u8,
 }
@@ -31,6 +33,12 @@ impl ReportCard {
     }
 }
 
+impl fmt::Display for ReportCard {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} ({}) - achieved a grade of {}", self.student_name, self.student_age, "A+")
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -38,7 +46,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "2.1".to_string(),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -52,7 +60,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
